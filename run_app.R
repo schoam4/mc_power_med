51d2e5d9a99e390b6038ae622f1d73a3ff4a3a73
Aaron Boulton <aboulton@email.unc.edu>
2016-08-31 12:40:42 -0400

    Miscellaneous Updates pt. 2

--- run_app.R
+++ run_app.R
@@ -1,5 +1,5 @@
-# The "shiny" "shinyBS" and "MASS" packages need to be installed. shinyBS and MASS are loaded
-# automatically during the App generation.
+# The "shiny" and "MASS" packages need to be installed. The MASS package is
+# loaded automatically during the App generation.
 
 # install.packages("shiny")
 # install.packages("MASS")
@@ -8,4 +8,4 @@ library(shiny)
 # Run app in "showcase" mode. This should open it in a separate window
 # or your browser, and you should be able to see the ui.R and server.R
 # files next to the app.
-runApp("../mc_power", display.mode = "normal")
+runApp("../mc_power_med", display.mode = "normal")
